366f182dabbb96cb9438d659f21e853dfab06583
orestispanago <orestispanago@gmail.com>
2021-06-12 18:46:52 +0300

    Added missing credentials  file

---
+++ src/credentials.h
@@ -0,0 +1,12 @@
+#ifndef credentials_h
+#define credentials_h
+
+const char* WiFi_SSID = "YourWiFiSSID";
+const char* WiFi_PW = "YourWiFiPassword";
+const char* mqtt_broker = "YourMQTTBrokerIP";
+const char* mqtt_user = "YourMQTTBrokerUsername";
+const char* mqtt_pw = "YourMQTTBrokerPassword";
+const char* input_topic = "YourTopic";
+
+
+#endif
